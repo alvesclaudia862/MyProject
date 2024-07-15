9463909cfdd8aee34160dd419d2cbda8986ba898
alvesclaudia862 <alvesclaudia862@gmail.com>
2024-07-15 09:37:49 -0300

    Add files via upload

---
+++ MyActivity.py
@@ -0,0 +1,9 @@
+#Demonstração de uso de operadores...
+#Construir um programa somatório de dois números
+
+
+x = int(input("Digite o valor 1 :"))
+y = int(input("Digite o valor 2:"))
+soma = x + y 
+print("O resultado da operação 3 é :", soma)
+

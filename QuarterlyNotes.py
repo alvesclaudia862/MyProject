cc03a9ac5a26136651a20b6d7148dd3299c6b0bb
alvesclaudia862 <alvesclaudia862@gmail.com>
2024-07-17 11:25:17 -0300

    Add files via upload

---
+++ QuarterlyNotes.py
@@ -0,0 +1,18 @@
+#Construa um programa onde o usuário e programa irá calcular a média 
+print("Digite sua média")
+nota1 = int(input("Digite a nota 1:"))
+nota2 = int(input("Digite a nota 2:"))
+nota3 = int(input("Digite a nota 3:"))
+nota4 = int(input("Digite a nota 4:"))
+média = (nota1 + nota2 + nota3 + nota4) / 4
+
+
+if média >= 6:
+    print("Você está Aprovado")
+else: 
+   print ("Você está reprovado...")
+
+
+  
+
+  

af8620f934b7131cf47589ed2fbd1593162fdd9f
alvesclaudia862 <alvesclaudia862@gmail.com>
2024-07-17 11:30:16 -0300

    Add files via upload

---
+++ MyDecisions.py
@@ -0,0 +1,21 @@
+#Demostração de operadores lógicos em condicionais....
+print("O que você vai fazer amanhã de manhã?")
+print("dornir / estudar / planejar")
+manha = input()
+print("O que você vai fazer amanhã de tarde?")
+print("jogar / treinar / trabalhar")
+tarde = input()
+
+if not manha or not tarde :
+    print ("Você precisa dizer o que vai fazer!")
+else:
+    if manha == "dormir" or tarde == "jogar":
+        print("Você está disperdiçando o seu dia!")
+    elif manha == "estudar" or tarde == "treinar":    
+        print("Que bom! Você irá se aperfeiçoar!")
+    elif manha =="planejar" and tarde == "trabalhar":
+        print("Para trabalhar melhor, devo planejar!")   
+    else:
+        print ("Não combinamos estas ações...")
+
+        print("Tenha um bom dia")

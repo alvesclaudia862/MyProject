2dc69704faa29491a140f52c0d6ae7872b7b5fbf
Claudia Alves <alvesclaudia862@gmail.com>
2024-08-16 11:45:53 -0300

    Add files via upload

---
+++ MyPolymorphism.py
@@ -0,0 +1,29 @@
+#Demonstração de OOP em Python...
+from abc import ABC, abstractmethod
+
+class cliente:
+    @abstractmethod
+    def __init__ (self, titular, conta, saldo):
+        self.titular = titular
+        self.conta = conta
+        self.saldo = saldo 
+
+    def apresentar(self):
+        print ("Olá! Eu sou a classe-pai") 
+        pass
+
+class cliente_fisico(cliente):
+    def apresentar(self):
+        print("Olá! Eu sou:", self.titular) 
+        print("Minha conta é:", self.conta)
+        print("E quero saber o meu saldo. ")
+  
+#Para criar uma instância baseada na classe Cliente ...
+fulano = cliente_fisico("João", "423.050205-21", 25000.00)
+
+#Executando o método da instância criada...
+fulano.apresentar()
+
+#Acessando os atributos das instâncias criadas...
+print("De fato, você realmente é: ", fulano.titular)
+print("No momento, a sua conta possui R$", fulano.saldo)
